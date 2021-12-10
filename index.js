464390ef8317670af4b35fbd1e5fb9f12ef35a32
Anubhav Sahoo <sahoo@Anubhavs-MacBook-Pro.local>
2021-12-10 18:27:02 +0530

    added photo library upgrade fixes

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ import {
   TouchableOpacity,
   Platform
 } from 'react-native'
-import { launchCamera } from 'react-native-image-picker'
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker'
 import ImageResizer from 'react-native-image-resizer'
 import RNFS from 'react-native-fs'
 
 export default class PhotoUpload extends React.Component {
   static propTypes = {
+    launchImageLibraryFirst: PropTypes.boolean,
     containerStyle: PropTypes.object,
     photoPickerTitle: PropTypes.string,
     maxHeight: PropTypes.number,
@@ -52,69 +53,74 @@ export default class PhotoUpload extends React.Component {
     if (this.props.onStart) this.props.onStart()
 
     // get image from image picker
-    launchCamera(this.options, async response => {
+    let launchFn = launchCamera;
+    if(this.props.launchImageLibraryFirst) {
+      launchFn = launchImageLibrary
+    }
+    launchFn(this.options, async response => {
       try {
-      this.setState({buttonDisabled: false})
+        this.setState({buttonDisabled: false})
 
-      let rotation = 0 
-      const {originalRotation} = response
-      
+        let rotation = 0 
+        const {originalRotation} = response
+        
 
-      if (this.props.onResponse) this.props.onResponse(response)
+        if (this.props.onResponse) this.props.onResponse(response)
 
-      if (response.didCancel) {
-        console.log('User cancelled image picker')
-        if (this.props.onCancel) this.props.onCancel('User cancelled image picker')
-        return
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error)
-        if (this.props.onError) this.props.onError(response.error)
-        return
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton)
-        if (this.props.onTapCustomButton) this.props.onTapCustomButton(response.customButton)
-        return
-      }
+        if (response.didCancel) {
+          console.log('User cancelled image picker')
+          if (this.props.onCancel) this.props.onCancel('User cancelled image picker')
+          return
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error)
+          if (this.props.onError) this.props.onError(response.error)
+          return
+        } else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton)
+          if (this.props.onTapCustomButton) this.props.onTapCustomButton(response.customButton)
+          return
+        }
 
-      let { maxHeight, maxWidth, quality, format } = this.state
-      
-      //Determining rotation param
-      if ( originalRotation === 90) { 
-        rotation = 90 
-      } else if (originalRotation === 180) { 
-        //For a few images rotation is 180. 
-        rotation = -180 
-      } else if ( originalRotation === 270 )  {
-        //When taking images with the front camera (selfie), the rotation is 270.
-        rotation = -90 
-      }
-      // resize image
-      const resizedImageUri = await ImageResizer.createResizedImage(
-        response.uri, // `data:image/jpeg;base64,${response.data}`,
-        maxHeight,
-        maxWidth,
-        format,
-        quality,
-        rotation
-      )
+        let { maxHeight, maxWidth, quality, format } = this.state
+        
+        //Determining rotation param
+        if ( originalRotation === 90) { 
+          rotation = 90 
+        } else if (originalRotation === 180) { 
+          //For a few images rotation is 180. 
+          rotation = -180 
+        } else if ( originalRotation === 270 )  {
+          //When taking images with the front camera (selfie), the rotation is 270.
+          rotation = -90 
+        }
+        // resize image
+        const [asset0] = response.assets; // FIXME: handles only 1 image
+        const resizedImageUri = await ImageResizer.createResizedImage(
+          asset0.uri, // `data:image/jpeg;base64,${response.data}`,
+          maxHeight,
+          maxWidth,
+          format,
+          quality,
+          rotation
+        )
 
-      if (this.props.onResizedImageUri) this.props.onResizedImageUri(resizedImageUri)
+        if (this.props.onResizedImageUri) this.props.onResizedImageUri(resizedImageUri)
 
-      /*
-      const filePath = Platform.OS === 'android' && resizedImageUri.uri.replace
-        ? resizedImageUri.uri.replace('file:/data', '/data')
-        : resizedImageUri.uri
+        /*
+        const filePath = Platform.OS === 'android' && resizedImageUri.uri.replace
+          ? resizedImageUri.uri.replace('file:/data', '/data')
+          : resizedImageUri.uri
 
-      // convert image back to base64 string
-      const photoData = await RNFS.readFile(filePath, 'base64')
-      let source = { uri: resizedImageUri.uri }
-      this.setState({
-        avatarSource: source
-      })
+        // convert image back to base64 string
+        const photoData = await RNFS.readFile(filePath, 'base64')
+        let source = { uri: resizedImageUri.uri }
+        this.setState({
+          avatarSource: source
+        })
 
-      // handle photo in props functions as data string
-      if (this.props.onPhotoSelect) this.props.onPhotoSelect(photoData)
-      */
+        // handle photo in props functions as data string
+        if (this.props.onPhotoSelect) this.props.onPhotoSelect(photoData)
+        */
       } catch(err) {
         if (this.props.onError) this.props.onError(err);
       }
